df49fc5db2a0c4dd4bc4241ac9fc3fbe513ee1d5
Shashwat Shukla <167205351+shashwatshukla11@users.noreply.github.com>
2025-06-01 21:19:30 +0530

    Create distribute-candies-among-children-ii.java

---
+++ Day01/distribute-candies-among-children-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long distributeCandies(int n, int limit) 
+    {
+        int minCh1 = Math.max(0, n-(2*limit));
+		int maxCh1 = Math.min(n, limit);
+		
+		long ways = 0;
+        
+        for(int i=minCh1;i<=maxCh1;i++)
+        {
+        	int remaining = (n-i);
+        	
+        	int minCh2 = Math.max(0, remaining-limit);
+        	int maxCh2 = Math.min(remaining, limit);
+        	
+        	ways += (maxCh2-minCh2+1);
+        }
+        
+        return ways;        
+    }
+}
